a1cbf46758f0effe2cf7b83070150e042670544e
mornac <imornac@gmail.com>
2025-09-22 16:07:02 +0200

    Fix try to find in main and delete the error to fix

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,12 +43,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->next = (*head)->next;
-		free(*head);
+		if ((*head)->prev)
+			(*head)->prev->next = (*head)->next;
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
+		free(*head);
 		*head = saved_head;
 	}
 	return (1);
 }
-
